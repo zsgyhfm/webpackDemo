0bcbeb9b96cb034e92352a67250cac375948b359
xiaoyu <xkas@gmail.com>
2019-04-13 14:23:32 +0800

    在index.js里面添加polyfill 使用es6的新特性API

--- src/base.js
+++ src/base.js
@@ -1,3 +1,24 @@
+import "@babel/polyfill" //导入这个框架 可以使用es6的新特性api
 let tag=()=>console.log('click');
+console.log(...[1,2,3,4]);
+//新特性 生成器
+function *nums(){
+    for (const key in [1,2,3,4,5]) {
+       yield key
+    }
+    return "end"
+}
+let aa = nums();
+let flag = true;
+while (flag){
+    let b =aa.next()
+    if (!b.done){
+        console.log(b);
+    }else{
+        console.log(b)
+        flag=false;
+    }
+}
 
 export default tag;
+export const {name,age} = {name:"zaks",age:123}
--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,14 @@ const path = require('path'); // nodejs的模块
 const HTMLWeabpackPligin = require("html-webpack-plugin");//导入模块 这个模块会自动吧出口文件放到打包文件夹中页面的script中,缺陷是每次都会删除之前的html新写入
 const CleanWebpackPlugin = require("clean-webpack-plugin");//在每次构建前清理 /dist 文件夹，这样只会生成用到的文件
 const webpack = require("webpack");
+
 module.exports = {
     //单入口文件配置
     // entry: "./src/index.js",
     //多入口配置
     entry:{
         // app:"./src/app.js",
-        index:'./src/index.js',
+        index:['./src/index.js'],
     },
     //输出文件 已入口文件名来命名之后打包的文件名 实现多入口多出口
     output: {
